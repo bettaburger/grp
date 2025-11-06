1d5ab9e1a1d4329c1d196936720304413fb45343
stephanie <stephanieha2021@gmail.com>
2025-11-06 11:33:13 -0500

    added debug helper

--- grp.go
+++ grp.go
@@ -1,5 +1,27 @@
-package grp 
+package main
+
+import (
+	"fmt"
+	//"os"
+	"github.com/pborman/getopt/v2"
+)
+
+var debug int
 
 func main() {
+	/**
+	Flag commands
+	Parse command arguments
+	Switch statement
+	*/
+	dFlag := getopt.Bool('d', "enable debug")	// -d debug
+	getopt.ParseV2()
 	
+	switch {
+	case *dFlag:
+		debug = 1
+		fmt.Println("debugging")	
+	default:
+		getopt.Usage()	
+	}	
 }
