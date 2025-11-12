01435ab023b7119c72346e7af37c32e00a21c3e2
stephanie <stephanieha2021@gmail.com>
2025-11-11 20:36:05 -0500

    environment variables

--- internal/shell/env.go
+++ internal/shell/env.go
@@ -1,13 +1,23 @@
 package shell
 
-/*envp*/
+import (
+	"os"
+	"fmt"
+)
 
+/*ENVP FOR PATHING*/
 type Envp struct {
-	Name string
-	Var string 
+	Name 	string
+	Var 	[]string 
 }
-
+/*ENVIRONMENT FUNCTION
+	Environment variables
+	Can display the name and variable(s)
+*/
 func Environment() {
-
-
+	env := os.Environ() // returns []string of "key=value"
+	for _, e := range env {
+		fmt.Println(e)
+	}
+	fmt.Printf("\n%d env variables\n", len(env))
 }
