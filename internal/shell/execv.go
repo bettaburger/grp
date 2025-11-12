f04030c8b55b7979ffebad60e7330ddfdcfed3a0
stephanie <stephanieha2021@gmail.com>
2025-11-11 20:37:38 -0500

    added if statements

--- internal/shell/execv.go
+++ internal/shell/execv.go
@@ -2,24 +2,36 @@ package shell
 
 import (
 	"fmt"
+
 	"grp/pkg/compare"
+	"grp/pkg/debug"	
 )
 
 /*EXECUTE FUNCTION
 	if statements for commands
 */
-func Execute(command string) {
-	for ((true)) {
-		// exit command
-		if compare.StrCmp(command, "exit") == 0 {
-			break
+func Execute(command string, line string) {
+	for {
 		// ls command
-		} else if compare.StrCmp(command, "ls") == 0 {
+		if compare.StrCmp(command, "ls") == 0 {
 			// call ls command from CMDS here
 			fmt.Printf("	run ls here..\n")
 			break
+		// grp help command
+		} else if compare.StrCmp(line, "grp help") == 0 {
+			debug.HelperList()
+			break
+		// echo command
+		} else if compare.StrCmp(command, "echo") == 0 {
+			fmt.Printf("	run echo here..\n")
+			break
+		// env command
+		} else if compare.StrCmp(command, "env") == 0{
+			Environment()
+			break
+		// unidentified command
 		} else {
-			fmt.Print("command not found try --help or -h\n")
+			fmt.Print("command not found try 'grp help'\n")		
 			break
 		}
 	}
