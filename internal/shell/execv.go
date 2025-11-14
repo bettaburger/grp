e3d13c8d5538ec69ce3cb8f9beaae1648940216d
stephanie <stephanieha2021@gmail.com>
2025-11-13 20:59:56 -0500

    env commands and cd commands

--- internal/shell/execv.go
+++ internal/shell/execv.go
@@ -5,7 +5,15 @@ import (
 	"strings"
 	"grp/pkg/compare"
 	"grp/pkg/debug"	
+	"grp/internal/env"
+	"grp/internal/cmds"
 )
+
+var (
+	key string
+	value string
+)
+
 /*in future, move similar commands to its own folder? reduces flow of if statements? :p
 	maybe keywords such as a command's first letter or something similar. 
 */
@@ -14,6 +22,7 @@ import (
 	if statements for commands
 */
 func Execute(command string, line string) {
+	args := strings.Fields(line)
 	for {
 		// ls command
 		if compare.StrCmp(command, "ls") == 0 {
@@ -28,18 +37,56 @@ func Execute(command string, line string) {
 		} else if compare.StrCmp(command, "echo") == 0 {
 			fmt.Printf("	run echo here..\n")
 			break
+
+/*PATHING COMMANDS*/
+		// pwd command
+		} else if compare.StrCmp(command, "pwd") == 0 {
+			fmt.Printf("%s\n", cmds.PWD())
+			break
+
+/*CD*/
+		} else if compare.StrCmp(command, "cd") == 0 {
+			// check if there is "". 
+			//home := env.GetEnv(args[1])
+			// otherwise run cd [directory]	
+			target := "~" // default: go to home
+			if len(args) > 1 {
+				target = args[1]
+			}
+			cmds.ChangeDir(target)
+			break
+		} else if compare.StrCmp(command, "cd ~") == 0 {
+			cmds.ChangeDir("~")
+		
+		} else if compare.StrCmp(command, "cd $HOME") == 0 {
+			cmds.ChangeDir("$HOME")
+
+/*ENVIRONMENT COMMANDS*/
 		// env command
 		} else if compare.StrCmp(command, "env") == 0{
 			// prints all environment variables
-			Env()
+			env.Env()
 			break
 		// getenv command
 		} else if compare.StrCmp(command, "getenv") == 0 {
 			// gets the environment
-			args := strings.Fields(line)
-			GetEnv(args[1])
+			e := env.GetEnv(args[1])
+			fmt.Printf("%s\n", e)
 			break
-		// unidentified command
+		// setenv command
+		} else if compare.StrCmp(command, "setenv") == 0 {
+			key = args[1]
+			value = strings.Join(args[2:], " ")
+			env.SetEnv(key, value)
+			break
+		// unsetenv command
+		} else if compare.StrCmp(command, "unsetenv") == 0 {
+			key = args[1]
+			env.UnsetEnv(key)
+			//fmt.Println(len(env)) //==> checks to see if the env array is getting decremented by every unset
+			break
+
+/*IF IT IS NOT A COMMAND*/
 		} else {
 			fmt.Print("command not found try 'grp help'\n")		
 			break
