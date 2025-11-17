b65afe6382e2a2f72a7a1d505b36103eef3aa48e
stephanie <stephanieha2021@gmail.com>
2025-11-16 20:22:35 -0500

    os.Read works with reading the current path '.', aiming to make switch stmts similar to cd..

--- internal/cmds/ls.go
+++ internal/cmds/ls.go
@@ -1,1 +1,34 @@
 package cmds
+
+import (
+	"fmt"
+	"os"
+)
+
+/* take the current PWD, and list out the contents. */
+func List(target string) {
+
+	switch target {
+		// LISTS UNDER HOME, doesn't matter which directory you are in.
+		// tested already with linux
+		case "~", "$HOME":
+
+		// LISTS UNDER CURRENT DIRECTORY
+		case ".", "":
+
+		default:
+			// LISTS A SPECIFIC DIRECTORY/PATH, doesn't matter which directory you are in
+			// PATH OR DIRECTORY NOT FOUND, print error. "no such file or directory"
+	}
+		
+	
+	files, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	// lists out files/directories
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
